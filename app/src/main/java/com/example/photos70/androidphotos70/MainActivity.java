8c3edd4c6558d9b002749af5fad7e1d186858f9a
Sarah Squillace <ses333@scarletmail.rutgers.edu>
2018-12-08 14:49:50 -0500

    Made the welcome screen lead to the home activity

--- app/src/main/java/com/example/photos70/androidphotos70/MainActivity.java
+++ app/src/main/java/com/example/photos70/androidphotos70/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.photos70.androidphotos70;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startBtn = (Button) findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
