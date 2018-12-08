ac60965b0efeee8904fd901624f9a1a5d393f9ea
Sarah Squillace <ses333@scarletmail.rutgers.edu>
2018-12-08 17:34:23 -0500

    Started implementing onclicklisteners for menu items in home activity

--- app/src/main/java/com/example/photos70/androidphotos70/HomeActivity.java
+++ app/src/main/java/com/example/photos70/androidphotos70/HomeActivity.java
@@ -1,9 +1,14 @@
 package com.example.photos70.androidphotos70;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 public class HomeActivity extends Activity {
 
@@ -19,4 +24,57 @@ public class HomeActivity extends Activity {
         inflater.inflate(R.menu.home_menu, menu);
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.addItem:
+                createAlbumDialog();
+                break;
+            case  R.id.deleteItem:
+
+                break;
+            case R.id.renameItem:
+
+                break;
+            case R.id.searchItem:
+
+                break;
+            case R.id.openItem:
+
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void createAlbumDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("New Album");
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        builder.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String albumName = input.getText().toString();
+                createAlbum(albumName);
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void createAlbum(String albumName) {
+
+    }
+
 }
