3aa81e1f45202e3687c4fe46326a8619c9e53601
Sarah Squillace <ses333@scarletmail.rutgers.edu>
2018-12-08 16:37:42 -0500

    Added menu items on home activity

--- app/src/main/java/com/example/photos70/androidphotos70/HomeActivity.java
+++ app/src/main/java/com/example/photos70/androidphotos70/HomeActivity.java
@@ -2,6 +2,8 @@ package com.example.photos70.androidphotos70;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class HomeActivity extends Activity {
 
@@ -9,5 +11,12 @@ public class HomeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_menu, menu);
+        return true;
     }
 }
