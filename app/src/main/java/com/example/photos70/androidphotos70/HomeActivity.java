639b7207eeb148998142c1fc0e71dd020a4dad48
Sarah Squillace <ses333@scarletmail.rutgers.edu>
2018-12-09 03:43:17 -0500

    Added comments to make code easier to understand

--- app/src/main/java/com/example/photos70/androidphotos70/HomeActivity.java
+++ app/src/main/java/com/example/photos70/androidphotos70/HomeActivity.java
@@ -23,70 +23,81 @@ import java.nio.file.FileAlreadyExistsException;
 import java.util.ArrayList;
 
 public class HomeActivity extends Activity {
+
     private ArrayList<Album> albumList = new ArrayList<Album>();
-    // array adapter here
     private ArrayAdapter<Album> albumArrayAdapter;
 
     private Album selectedAlbum;
-    private int selectedIndex;
 
     private ListView albumsListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        selectedAlbum = null;
-        Resources res = getResources();
-        albumList.add(new Album("a"));
-        // array adapter here
+
+        selectedAlbum = null; // no selection upon entering activity
+        albumList.add(new Album("test")); //TODO TEST ALBUM: DELETE ME LATER
         albumArrayAdapter = new ArrayAdapter<Album>(this, R.layout.albums_listview_detail, albumList);
         albumsListView = (ListView) findViewById(R.id.albumsListView);
-        // array adapter here
         albumsListView.setAdapter(albumArrayAdapter);
         albumsListView.setOnItemClickListener( (p,v,pos,id) -> selectAlbum(pos) );
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // I honestly have no idea what this means, I just found this online.
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.home_menu, menu);
         return true;
     }
 
+    /**
+     *This method is basically the directory for the menu items on the tool bar.
+     *The switch statement decides what to do depending on what menu item was pressed.
+     * @param item
+     * @return
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         switch (item.getItemId()){
-            case R.id.addItem:
+            case R.id.addItem: // User pressed add
                 createAlbumDialog();
                 break;
-            case R.id.deleteItem:
+            case R.id.deleteItem: // User pressed delete
                 if(selectedAlbum == null) {
                     noAlbumSelectedDialog();
                     break;
                 }
                 deleteAlbumDialog();
                 break;
-            case R.id.renameItem:
+            case R.id.renameItem: // User pressed rename
                 if(selectedAlbum == null) {
                     noAlbumSelectedDialog();
                     break;
                 }
                 renameAlbumDialog();
                 break;
-            case R.id.searchItem:
+            case R.id.searchItem: // User pressed search
                 break;
-            case R.id.openItem:
+            case R.id.openItem: // User pressed open
                 if(selectedAlbum == null) {
                     noAlbumSelectedDialog();
                     break;
                 }
                 break;
         }
-
+        // Not sure if this is needed
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * This method shows a dialog that has an EditText where the user can enter the
+     * name of the new album. The user can also cancel at any time.
+     */
     private void createAlbumDialog() {
+
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Enter album name");
         final EditText input = new EditText(this);
@@ -120,6 +131,13 @@ public class HomeActivity extends Activity {
         builder.show();
     }
 
+    /**
+     * This method first checks if the there is already an album with the same name as albumName
+     * and then it creates a new album with that name. The album is added to albumList, the ArrayList
+     * of albums, and then the albumArrayAdapter is updated so the ListView is updated.
+     * @param albumName
+     * @throws Exception when there is an album with the same name as albumName already
+     */
     private void createAlbum(String albumName) throws Exception {
         for(Album a: albumList) {
             if(a.getName().equals(albumName)) {
@@ -137,6 +155,9 @@ public class HomeActivity extends Activity {
         selectedAlbum = albumList.get(pos);
     }
 
+    /**
+     * Shows a dialog asking if the user really wants to delete the selected album.
+     */
     private void deleteAlbumDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Are you sure?");
@@ -164,6 +185,10 @@ public class HomeActivity extends Activity {
         builder.show();
     }
 
+    /**
+     * Removes selected album from the ArrayList of albums, updates the albumArrayAdapter, and
+     * resets the selection highlighter.
+     */
     private void deleteAlbum() {
 
         albumList.remove(selectedAlbum);
