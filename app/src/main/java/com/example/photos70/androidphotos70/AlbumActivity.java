4050922a7c9155533651606c159f5aabdbcaecd7
Sarah Squillace <ses333@scarletmail.rutgers.edu>
2018-12-09 19:07:23 -0500

    Started on GUI and code for album activity

--- app/src/main/java/com/example/photos70/androidphotos70/AlbumActivity.java
+++ app/src/main/java/com/example/photos70/androidphotos70/AlbumActivity.java
@@ -2,6 +2,9 @@ package com.example.photos70.androidphotos70;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.photos70.model.Album;
@@ -12,10 +15,15 @@ public class AlbumActivity extends Activity {
     private ArrayList<Album> albumList;
     private Album thisAlbum;
 
+    private ArrayList<ImageView> imageViewArrayList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album);
+
+        GridView gridView = findViewById(R.id.gridview);
+
 
         Bundle bundle = getIntent().getExtras();
         thisAlbum = (Album) bundle.getSerializable("album");
